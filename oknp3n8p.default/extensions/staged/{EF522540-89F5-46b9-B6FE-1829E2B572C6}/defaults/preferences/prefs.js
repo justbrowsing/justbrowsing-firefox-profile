cbf4ba045dc49fdece8c683782eb47da4b86675d
justbrowsing <developer4linux@gmail.com>
2013-12-18 19:30:01 -0800

    Update Firefox profile to match 20131218 release

---
+++ oknp3n8p.default/extensions/staged/{EF522540-89F5-46b9-B6FE-1829E2B572C6}/defaults/preferences/prefs.js
@@ -0,0 +1,8 @@
+/*****************************************************
+Localized Description
+http://kb.mozillazine.org/Localize_extension_descriptions
+
+Also see Bug 257155
+https://bugzilla.mozilla.org/show_bug.cgi?id=257155
+*****************************************************/
+pref("extensions.{EF522540-89F5-46b9-B6FE-1829E2B572C6}.description", "chrome://googlepreview/locale/googlepreview.properties");
---
+++ oknp3n8p.default/extensions/staged/{EF522540-89F5-46b9-B6FE-1829E2B572C6}/install.js
@@ -0,0 +1,6 @@
+initInstall("SearchPreview", "/firefox extensions/googlepreview", "0.0");
+
+alert("Sorry, this extension package is designed for Mozilla Firefox 0.9.\n"
+	+ "Please upgrade your browser to a newer version");
+
+cancelInstall();
